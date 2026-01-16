d34070846e7a809345515e8cb1831ed9622422bc
Vishnukr0007 <vishnukrishnankutty54@gmail.com>
2026-01-16 17:29:31 +0530

    feat: Implement a new search results page with infinite scrolling, dynamic product fetching based on URL query, and a no-data state.

--- client/src/pages/Searchpage.jsx
+++ client/src/pages/Searchpage.jsx
@@ -96,14 +96,14 @@ const Searchpage = () => {
             next={fetchNextPage}
             hasMore={page < totalPage}
             loader={
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-4 mt-4">
                 {Array.from({ length: 10 }).map((_, index) => (
                   <CardLoading key={index} />
                 ))}
               </div>
             }
           >
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-4">
               {data.map((p, index) => (
                 <CardProduct
                   key={p._id + "-search-" + index}
